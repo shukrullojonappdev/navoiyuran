b7927aaaef2969dd9e3d6c949644c691e38e557d
shukrullojondevnu <shukrullojon.devnu@gmail.com>
2024-02-29 14:30:59 +0500

    commited

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
+// import node from "@astrojs/node";
+import cloudflare from "@astrojs/cloudflare";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
+  // output: "server",
+  // adapter: node({
+  //   mode: "standalone",
+  // }),
   output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: cloudflare(),
   integrations: [tailwind()],
 });
